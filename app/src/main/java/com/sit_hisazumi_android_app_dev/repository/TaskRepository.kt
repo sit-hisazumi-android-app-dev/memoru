a636ee4a47fccfd309f837035ce2c4aa97b7a425
10fu3 <msf.1192@gmail.com>
2022-11-21 16:12:29 +0900

    add repository interface

--- app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
+++ app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
@@ -1,8 +1,9 @@
 package com.sit_hisazumi_android_app_dev.repository
 
 import com.sit_hisazumi_android_app_dev.entity.Task
+import kotlinx.coroutines.flow.Flow
 
 interface ITaskRepository {
     fun save(tasks: List<Task>)
-    fun findAll() :List<Task>
+    fun findAll() : Flow<List<Task>>
 }

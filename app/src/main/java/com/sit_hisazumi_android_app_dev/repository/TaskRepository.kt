fa291b456b7c53a11d567a45b988a11935d1dc6a
10fu3 <msf.1192@gmail.com>
2022-11-21 16:19:57 +0900

    add memo

--- app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
+++ app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
@@ -4,6 +4,8 @@ import com.sit_hisazumi_android_app_dev.entity.Task
 import kotlinx.coroutines.flow.Flow
 
 //MemoRepository と TodoRepositoryの2種類のレポジトリクラスを用意して、それぞれITaskRepositoryを実装する
+//MemoRepositoryのPreferences DataStore Nameは"memo_repository"
+//TodoRepositoryのPreferences DataStore Nameは"todo_repository"
 interface ITaskRepository {
     fun save(tasks: List<Task>)
     fun findAll() : Flow<List<Task>>

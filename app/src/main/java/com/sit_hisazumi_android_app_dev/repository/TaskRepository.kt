318f61aa8f490cadbc913d64259e2198ebac803d
髙橋 允治 <bp20036@shibaura-it.ac.jp>
2022-12-05 16:53:38 +0900

    impl repository

--- app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
+++ app/src/main/java/com/sit_hisazumi_android_app_dev/repository/TaskRepository.kt
@@ -1,22 +1,89 @@
 package com.sit_hisazumi_android_app_dev.repository
 
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.emptyPreferences
+import androidx.datastore.preferences.preferencesKey
 import com.sit_hisazumi_android_app_dev.entity.Task
 import com.sit_hisazumi_android_app_dev.entity.TaskKind
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.IOException
 import java.time.LocalDateTime
 
 //MemoRepository と TodoRepositoryの2種類のレポジトリクラスを用意して、それぞれITaskRepositoryを実装する
 //MemoRepositoryのPreferences DataStore Nameは"memo_repository"
 //TodoRepositoryのPreferences DataStore Nameは"todo_repository"
 interface ITaskRepository {
-    fun save(tasks: List<Task>)
+    suspend fun save(tasks: List<Task>)
     fun findAll() : Flow<List<Task>>
+}
+
+class MemoRepository: ITaskRepository {
+    override suspend fun save(tasks: List<Task>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(): Flow<List<Task>> {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class TodoRepository: ITaskRepository {
+    lateinit var dataStore: DataStore<Preferences>;
+
+    //ここにMainActivity.ktからのcreateDataStoreが入る
+    constructor(dataStore: DataStore<Preferences>){
+        this.dataStore = dataStore
+    }
+
+    //TODOまたはMEMOをキーとする
+    private object PreferenceKeys {
+        val TaskKindName = preferencesKey<String>("ToDo")
+    }
+
+    //jsonContentはjson文字列化されたデータ
+    private suspend fun updateName(jsonContent: String) {
+        dataStore.edit {
+            it[PreferenceKeys.TaskKindName] = jsonContent
+        }
+    }
+
+    //記録
+    override suspend fun save(tasks: List<Task>) {
+        var hoge = tasks
+        var jsonContent = Json.encodeToString(hoge)
+
+        updateName(jsonContent)
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(): Flow<List<Task>> {
+        val preferencesFlow: Flow<String> = dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }.map { preferences ->
+                preferences[PreferenceKeys.TaskKindName] ?: "Not Found"
+            }
+
+        TODO("Not yet implemented")
+    }
+
 }
 
 //memo mock
 class MemoRepositoryMock: ITaskRepository{
-    override fun save(tasks: List<Task>) {
+    override suspend fun save(tasks: List<Task>) {
 
     }
 
@@ -34,10 +101,16 @@ class MemoRepositoryMock: ITaskRepository{
 
 //memo mock
 class TodoRepositoryMock: ITaskRepository{
-    override fun save(tasks: List<Task>) {
+    lateinit var dataStore: DataStore<Preferences>
+
+    //書き込み
+    override suspend fun save(tasks: List<Task>) {
+        var hoge = tasks
+        var jsonContent = Json.encodeToString(hoge)
 
     }
 
+    //読み込み＆表示
     override fun findAll(): Flow<List<Task>> {
         return flow {
             listOf(

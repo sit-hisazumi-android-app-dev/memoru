cd7d7dd58c45eff15e1c9318bd48840f6026a809
10fu3 <msf.1192@gmail.com>
2023-01-22 23:56:40 +0900

    テーマの適応

--- app/src/main/java/com/sit_hisazumi_android_app_dev/MainActivity.kt
+++ app/src/main/java/com/sit_hisazumi_android_app_dev/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MemoruTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),

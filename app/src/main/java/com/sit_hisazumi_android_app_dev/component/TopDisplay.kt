42bb29df605c2531688ee0678cc5aa2fabf1acb0
10fu3 <msf.1192@gmail.com>
2023-01-23 01:07:07 +0900

    削除時にリロードする

--- app/src/main/java/com/sit_hisazumi_android_app_dev/component/TopDisplay.kt
+++ app/src/main/java/com/sit_hisazumi_android_app_dev/component/TopDisplay.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.sit_hisazumi_android_app_dev.entity.Task
 import com.sit_hisazumi_android_app_dev.repository.*
 
 @Composable
@@ -23,17 +24,31 @@ fun Display(){
     val todoSource = TodoRepository(context.dataStore)
     val memoSource = MemoRepository(context.dataStore)
 
-    var todoList = todoSource.findAll().collectAsState(initial = listOf()).value
-    var memoList = memoSource.findAll().collectAsState(initial = listOf()).value
+    val todoList = mutableListOf<Task>()
+    var memoList = mutableListOf<Task>()
+
+    val reload:@Composable () -> Unit = {
+        todoList.clear()
+        memoList.clear()
+        todoSource.findAll().collectAsState(initial = listOf()).value.forEach{t->
+            todoList.add(t)
+        }
+        memoSource.findAll().collectAsState(initial = listOf()).value.forEach{t->
+            memoList.add(t)
+        }
+    }
+
+    todoList.addAll(todoSource.findAll().collectAsState(initial = listOf()).value)
+    memoList.addAll(memoSource.findAll().collectAsState(initial = listOf()).value)
 
     Column {
         Box(contentAlignment = Alignment.BottomCenter){
             Column(modifier = Modifier.fillMaxHeight()) {
                 ChangeTab(tabIndex = tabIndex, onTabIndexChange = {tabIndex = it})
                 if(tabIndex == 0){
-                    TodoList(todoList,todoSource)
+                    TodoList(todoList,todoSource,reload)
                 }else{
-                    MemoList(memoList,memoSource)
+                    MemoList(memoList,memoSource,reload)
                 }
             }
             if(tabIndex == 1){
